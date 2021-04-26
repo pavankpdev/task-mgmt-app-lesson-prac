c0c7d41f8437fd17b629709515ab9440ea987298
pavankpdev <pavankumarkpkp@gmail.com>
2021-04-26 12:29:47 +0530

    add delete task feature

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,7 @@ const state = {
 };
 
 const taskContents = document.querySelector(".task__contents");
+const taskModal = document.querySelector(".task__modal__body");
 
 const htmlTaskContent = ({
   id,
@@ -10,16 +11,16 @@ const htmlTaskContent = ({
   description,
   type,
   url,
-}) => ` <div class="col-md-6 col-lg-4 mt-3" id=${id}>
+}) => ` <div class="col-md-6 col-lg-4 mt-3" id=${id} key=${id}>
 <div class="card shadow-sm task__card">
   <div
     class="card-header d-flex justify-content-end task__card__header"
   >
-    <button type="button" class="btn btn-outline-info mr-2">
+    <button type="button" class="btn btn-outline-info mr-2" id=${id}>
       <i class="fas fa-pencil-alt"></i>
     </button>
-    <button type="button" class="btn btn-outline-danger">
-      <i class="fas fa-trash-alt"></i>
+    <button type="button" class="btn btn-outline-danger" id=${id} onclick="deleteTask.apply(this, arguments)">
+      <i class="fas fa-trash-alt" id=${id}></i>
     </button>
   </div>
   <div class="card-body">
@@ -42,12 +43,32 @@ const htmlTaskContent = ({
       class="btn btn-outline-primary float-right"
       data-bs-toggle="modal"
       data-bs-target="#showTask"
+      onclick="openTask.apply(this, arguments)"
+      id=${id}
     >
       Open Task
     </button>
   </div>
 </div>
 </div>`;
+
+const htmlModalContent = ({ id, title, description, type, url }) => {
+  const date = new Date(id);
+  return ` <div id=${id}>
+  <img
+  src=${
+    url ||
+    `https://images.unsplash.com/photo-1572214350916-571eac7bfced?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=755&q=80`
+  }
+  alt="bg image"
+  class="img-fluid place__holder__image mb-3"
+  />
+  <strong class="text-sm text-muted">Created on ${date.toDateString()}</strong>
+  <h2 class="my-3">${title}</h2>
+  <p class="lead">
+  ${description}
+  </p></div>`;
+};
 
 const updateLocalStorage = () => {
   localStorage.setItem("tasky", JSON.stringify({ tasks: state.taskList }));
@@ -62,6 +83,7 @@ const loadInitialData = () => {
 };
 
 const handlesubmit = (e) => {
+  const id = `${Date.now()}`;
   const input = {
     url: document.getElementById("imageUrl").value,
     title: document.getElementById("taskTitle").value,
@@ -69,9 +91,34 @@ const handlesubmit = (e) => {
     type: document.getElementById("Tags").value,
   };
 
-  input.id = `${Date.now()}`;
-
-  taskContents.insertAdjacentHTML("beforeend", htmlTaskContent(input));
-  state.taskList.push(input);
+  taskContents.insertAdjacentHTML(
+    "beforeend",
+    htmlTaskContent({ ...input, id })
+  );
+  state.taskList.push({ ...input, id });
   updateLocalStorage();
+};
+
+const openTask = (e) => {
+  if (!e) e = window.event;
+
+  const getTask = state.taskList.filter(({ id }) => id === e.target.id);
+  taskModal.innerHTML = htmlModalContent(getTask[0]);
+};
+
+const deleteTask = (e) => {
+  if (!e) e = window.event;
+  const targetID = e.target.id;
+  const type = e.target.tagName;
+  const removeTask = state.taskList.filter(({ id }) => id !== targetID);
+  state.taskList = removeTask;
+
+  updateLocalStorage();
+  if (type === "BUTTON")
+    return e.target.parentNode.parentNode.parentNode.parentNode.removeChild(
+      e.target.parentNode.parentNode.parentNode
+    );
+  return e.target.parentNode.parentNode.parentNode.parentNode.parentNode.removeChild(
+    e.target.parentNode.parentNode.parentNode.parentNode
+  );
 };
